49d5cacbed7510afe479b587ef8b64f365bcd194
nsafarova <nigarsafarova@hotmail.com>
2023-04-15 18:55:15 +0400

    style: :lipstick: setup Tailwind CSS custom theme
    setup Tailwind CSS custom theme (colors, fonts, spacing, dark mode)

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,66 @@ module.exports = {
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
+  darkMode: false, // or 'media(set based on OS)' or 'class'
   theme: {
-    extend: {},
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
+    },
+    fontFamily: {
+      display: ["Sora", "system-ui", "sans-serif"],
+      body: ["Inter", "system-ui", "sans-serif"],
+    },
+    colors: {
+      purple: {
+        50: "#F5F3FF",
+        100: "#EDE9FE",
+        200: "#DDD6FE",
+        300: "#C4B5FD",
+        400: "#A78BFA",
+        500: "#8B5CF6",
+        600: "#7C3AED",
+        700: "#6D28D9",
+        800: "#5B21B6",
+        900: "#4C1D95",
+      },
+      neutral: {
+        50: "#FAFAFA",
+        100: "#F5F5F5",
+        200: "#E5E5E5",
+        300: "#D4D4D4",
+        400: "#A3A3A3",
+        500: "#737373",
+        600: "#525252",
+        700: "#404040",
+        800: "#262626",
+        900: "#171717",
+      },
+    },
+    fontSize: {
+      "2xs": "0.75rem",
+      xs: "0.875rem",
+      sm: "1rem",
+      base: "1.125rem",
+      lg: "1.25rem",
+      xl: "1.5rem",
+      "2xl": "1.75rem",
+      "3xl": "2rem",
+      "4xl": "3rem",
+      "5xl": "3.5rem",
+    },
+    extend: {
+      spacing: {
+        24: "1.5rem",
+        120: "7.5rem",
+      },
+      borderRadius: {
+        default: "0.3125rem",
+      },
+    },
   },
   plugins: [],
 };
