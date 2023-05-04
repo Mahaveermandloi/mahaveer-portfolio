caad6a98a34cecae776e3053a3ebde368682dfcb
nsafarova <nigarsafarova@hotmail.com>
2023-05-05 01:43:25 +0400

    chore: :speech_balloon: add project card contents
    Added project card content: titles, images, descriptions, teck stack and links.

--- components/projectCard/index.js
+++ components/projectCard/index.js
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
+import dynamic from "next/dynamic";
 
 const Project = ({
   title,
@@ -11,8 +12,8 @@ const Project = ({
 }) => {
   return (
     <div className="max-w-lg bg-neutral-50 dark:bg-neutral-900 border border-neutral-200 dark:border-neutral-700 overflow-hidden rounded-sm z-0">
-      <div className="relative h-72 w-full z-1">
-        <Image src={imageUrl} alt={title} fill />
+      <div className="relative">
+        <Image src={imageUrl} alt={title} responsive width={510} height={300}  />
       </div>
       <div className="relative z-1 px-4 py-2 bg-neutral-50 dark:bg-neutral-900">
         <h3 className="font-display text-lg md:text-2xl text-neutral-900 dark:text-neutral-50 font-bold my-2">
@@ -21,7 +22,7 @@ const Project = ({
         <p className="font-body text-xs md:text-sm text-neutral-600 dark:text-neutral-300">
           {description}
         </p>
-        <div className="flex my-4">
+        <div className="flex my-5">
           {techStackIcons.map((techStackIcon) => (
             <span
               key={techStackIcon.key}
@@ -56,4 +57,4 @@ const Project = ({
   );
 };
 
-export default Project;
+export default dynamic (() => Promise.resolve(Project), {ssr: false})
--- data/projects.js
+++ data/projects.js
@@ -6,58 +6,59 @@ import {
   SiTailwindcss,
   SiNextdotjs,
   SiFigma,
-  SiStorybook,
 } from "react-icons/si";
 
 const projects = [
   {
-    title: "My Awesome Project",
+    title: "E-commerce Shop",
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean euismod bibendum laoreet.",
-    imageUrl: "/images/wallpaper.png",
-    githubUrl: "https://github.com/yourusername/my-awesome-project",
-    liveDemoUrl: "https://my-awesome-project.netlify.app/",
+      "A responsive e-commerce website developed with React and utilizing the Fake Store API for data, while employing the Context API for state management.",
+    imageUrl: "/images/project-1.jpg",
+    githubUrl: "https://github.com/nsafarova/ecommerce-shop-react-app",
+    liveDemoUrl: "https://ecommerce-shop-react-app.vercel.app/",
     techStackIcons: [
       { icon: <SiReact />, key: "react" },
+      { icon: <SiJavascript />, key: "javascript" },
       { icon: <SiTailwindcss />, key: "tailwind" },
+    ],
+  },
+  {
+    title: "Image Gallery",
+    description:
+      "An image gallery app that utilizes the Unsplash API and is built with React, featuring a wide variety of high-quality photos to choose from.",
+    imageUrl: "/images/project-2.jpg",
+    githubUrl: "https://github.com/nsafarova/image-gallery-react-app",
+    liveDemoUrl: "https://nsafarova.github.io/image-gallery-react-app/",
+    techStackIcons: [
+      { icon: <SiReact />, key: "react" },
+      { icon: <SiJavascript />, key: "javascript" },
+      { icon: <SiTailwindcss />, key: "tailwind" },
+    ],
+  },
+  {
+    title: "Agency Landing Page",
+    description:
+      "The agency landing page, built with ReactJS, features a responsive design optimized for both desktop and mobile viewing.",
+    imageUrl: "/images/project-3.jpg",
+    githubUrl: "https://github.com/nsafarova/landing-page-design",
+    liveDemoUrl: "https://nsafarova.github.io/landing-page-design/",
+    techStackIcons: [
+      { icon: <SiReact />, key: "react" },
+      { icon: <SiCss3 />, key: "css" },
       { icon: <SiFigma />, key: "figma" },
-      { icon: <SiTailwindcss />, key: "nodejs" },
     ],
   },
   {
-    title: "My Awesome Project",
+    title: "Weather App",
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean euismod bibendum laoreet.",
-    imageUrl: "/images/wallpaper.png",
-    githubUrl: "https://github.com/yourusername/my-awesome-project",
-    liveDemoUrl: "https://my-awesome-project.netlify.app/",
+      "A weather app using OpenWeather API and built with React that displays weather information and changes the background based on the current weather condition.",
+    imageUrl: "/images/project-4.jpg",
+    githubUrl: "https://github.com/nsafarova/weather-app",
+    liveDemoUrl: "https://nsafarova.github.io/weather-app/",
     techStackIcons: [
       { icon: <SiReact />, key: "react" },
-      { icon: <SiTailwindcss />, key: "nodejs" },
-    ],
-  },
-  {
-    title: "My Awesome Project",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean euismod bibendum laoreet.",
-    imageUrl: "/images/wallpaper.png",
-    githubUrl: "https://github.com/yourusername/my-awesome-project",
-    liveDemoUrl: "https://my-awesome-project.netlify.app/",
-    techStackIcons: [
-      { icon: <SiReact />, key: "react" },
-      { icon: <SiTailwindcss />, key: "nodejs" },
-    ],
-  },
-  {
-    title: "My Awesome Project",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean euismod bibendum laoreet.",
-    imageUrl: "/images/wallpaper.png",
-    githubUrl: "https://github.com/yourusername/my-awesome-project",
-    liveDemoUrl: "https://my-awesome-project.netlify.app/",
-    techStackIcons: [
-      { icon: <SiReact />, key: "react" },
-      { icon: <SiTailwindcss />, key: "nodejs" },
+      { icon: <SiJavascript />, key: "javascript" },
+      { icon: <SiCss3 />, key: "css" },
     ],
   },
   // Add more projects here
