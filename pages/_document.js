67bc4abc1ad2c81a7c1f8ca66f207979d712c2ab
nsafarova <nigarsafarova@hotmail.com>
2023-05-10 22:55:44 +0400

    fix: :bug: fix font loading issue
    fix font loading issue

--- pages/_document.js
+++ pages/_document.js
@@ -4,7 +4,10 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+      <link href="https://fonts.googleapis.com/css2?family=Sora&display=swap" rel="stylesheet"></link>
+
+      </Head>
       <body>
         <Main />
         <NextScript />
