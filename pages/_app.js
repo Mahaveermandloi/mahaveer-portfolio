2ff77103b947a0037978c2a103ae6a51a4180f48
nsafarova <nigarsafarova@hotmail.com>
2023-05-10 22:06:52 +0400

    feat: :art: set default theme to light theme
    set default theme to light theme

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,10 @@
 import { ThemeProvider } from "next-themes";
 import 'tailwindcss/tailwind.css';
-import '../styles/globals.css';
+import '../styles/fonts.css';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ThemeProvider attribute="class">
+    <ThemeProvider defaultTheme="light" attribute="class">
       <Component {...pageProps} />
     </ThemeProvider>
   );
